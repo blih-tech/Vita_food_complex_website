f76597e4a5ddf987bff7c4a52e711411ee70c9fb
blih7 <blih.marketing2023@gmail.com>
2026-04-05 01:10:35 +0000

    feat: restore full about page

--- apps/frontend/src/app/[locale]/about/page.tsx
+++ apps/frontend/src/app/[locale]/about/page.tsx
@@ -1,7 +1,23 @@
+import AboutHeroSection from "@frontend/components/sections/AboutHeroSection";
+import AboutStorySection from "@frontend/components/sections/AboutStorySection";
+import AboutValuesSection from "@frontend/components/sections/AboutValuesSection";
+import AboutOwnersSection from "@frontend/components/sections/AboutOwnersSection";
+import TestimonialSection from "@frontend/components/sections/TestimonialSection";
+import CTASection from "@frontend/components/sections/CTASection";
+import Footer from "@frontend/components/layout/Footer";
+import Navbar from "@frontend/components/layout/Navbar";
+
 export default function AboutPage() {
   return (
     <main className="flex flex-col">
-      <h1>About Page</h1>
+      <Navbar />
+      <AboutHeroSection />
+      <AboutStorySection />
+      <AboutValuesSection />
+      <AboutOwnersSection />
+      <TestimonialSection />
+      <CTASection />
+      <Footer />
     </main>
   );
 }
