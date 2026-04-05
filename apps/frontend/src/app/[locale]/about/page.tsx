174c39f2c96805f03b5a8dd2aa3b1ef00ab9ac55
blih7 <blih.marketing2023@gmail.com>
2026-04-05 00:42:57 +0000

    fix: add Navbar to About page

--- apps/frontend/src/app/[locale]/about/page.tsx
+++ apps/frontend/src/app/[locale]/about/page.tsx
@@ -5,10 +5,12 @@ import AboutOwnersSection from "@frontend/components/sections/AboutOwnersSection
 import TestimonialSection from "@frontend/components/sections/TestimonialSection";
 import CTASection from "@frontend/components/sections/CTASection";
 import Footer from "@frontend/components/layout/Footer";
+import Navbar from "@frontend/components/layout/Navbar";
 
 export default function AboutPage() {
   return (
     <main className="flex flex-col">
+      <Navbar />
       <AboutHeroSection />
       <AboutStorySection />
       <AboutValuesSection />
